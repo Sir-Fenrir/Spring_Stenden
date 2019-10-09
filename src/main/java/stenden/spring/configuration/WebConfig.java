d91c42517bdd50a49643aceb9ff45ff5caa13e0e
Sander ten Hoor <sthoor@quintor.nl>
2019-10-09 10:39:27 +0200

    ENH: Removed unused import

--- src/main/java/stenden/spring/configuration/WebConfig.java
+++ src/main/java/stenden/spring/configuration/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 // This is a configuration class
 @Configuration
